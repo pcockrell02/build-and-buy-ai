573b47aeffb4a87a992e0fce00db03d1df628236
pcockrell02 <parker.cockrell02@gmail.com>
2026-02-19 15:15:04 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,19 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-```
-
-4. Commit changes
-
----
-
-### **FILE 3: `.gitignore`**
-
-1. Click "Add file" → "Create new file"
-2. Name: `.gitignore`
-3. Copy this:
-```
-node_modules/
-.next/
-.env*.local
-.vercel
